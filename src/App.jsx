9e40e00f7bb50b5f354d1267f7821b26e7b374a8
Som1326 <sebastianorregomarin@gmail.com>
2024-04-25 22:49:47 -0500

    Refactor styles, add GameRouter component, and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { OrbitControls } from "@react-three/drei";
+import { BakeShadows, OrbitControls } from "@react-three/drei";
+import Environments from "./game/globals/Environments";
 import World from './game/pages/level_3/World';
+import World2 from './game/pages/level_2/World';
+import Lights from "./game/pages/level_2/Lights";
+import { Perf } from "r3f-perf";
+import WelcomeText from "./game/pages/level_2/WelcomeText";
 
 const App = () => {
     return (
         <>
-            <ambientLight />
-            <directionalLight position={[10, 10, 5]} />
+            <Perf position="top-left" />
+            <Lights />
+            <BakeShadows />
+            <Environments />
             <OrbitControls makeDefault />
-            <World />
+            <World2>
+                <WelcomeText />
+            </World2>
+            {/* <ambientLight />
+            <directionalLight position={[10, 10, 5]} />
+            <World /> */}
         </>
 
     );
---
+++ src/game/pages/level_2/WelcomeText.jsx
@@ -0,0 +1,59 @@
+import { Center, Float, Html, Text, Text3D } from "@react-three/drei";
+
+const WelcomeText = (props) => {
+    const text = "Estacion Planetaria 2";
+
+    return (
+        <Float
+            speed={1.5}
+            rotationIntensity={0.1}
+            floatIntensity={0.5}
+            floatingRange={[0, 1]}
+        >
+
+            <Center position={[0, 0.8, -6.5]}>
+                <Text3D
+                    position={props.position}
+                    font={"/assets/fonts/MarsMissionFont.json"}
+                    bevelEnabled
+                    bevelSize={0.005}
+                    bevelThickness={0.01}
+                    height={0.1}
+                    letterSpacing={0.05}
+                    size={0.25}
+                >
+                    <meshDepthMaterial/>
+                    {text}
+                </Text3D>
+            </Center>
+        </Float>
+
+        // <Float
+        //     speed={1.5}
+        //     rotationIntensity={0.1}
+        //     floatIntensity={0.5}
+        //     floatingRange={[-1, 0]}
+        // >
+        //     <Text
+        //         position={props.position}
+        //         fontSize={0.2}
+        //         color={"orange"}
+        //         textAlign="center"
+        //         font="/assets/fonts/MarsMissionFont.ttf"
+        //     >
+        //         Estacion Planetaria 2
+        //     </Text>
+        // </Float>
+
+        //   <Html
+        //     className="welcome-text"
+        //     position={props.position}
+        //     distanceFactor={10}
+        //     occlude
+        //   >
+        //     <h2>Estacion Planetaria 2</h2> 
+        //     </Html>
+    )
+}
+
+export default WelcomeText;
