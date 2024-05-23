1669a639ab0b4cf84d3e71192d7e79acdd1b9afb
Som1326 <sebastianorregomarin@gmail.com>
2024-05-22 23:00:18 -0500

    Animaciones personaje y villano agregadas

--- src/game/pages/level_2/Level2.jsx
+++ src/game/pages/level_2/Level2.jsx
@@ -13,6 +13,7 @@ import Ecctrl from "ecctrl";
 import Avatar2 from "../../globals/player/Avatar2";
 import Logout from "../../../components/logout/Logout";
 import WorldS2 from "./world/WorldS2";
+import Villain2 from "../../globals/villains/Villain2";
 
 export const Level2 = (props) => {
     const map = useMovements();
@@ -30,22 +31,26 @@ export const Level2 = (props) => {
                 <Suspense fallback={null} >
                     <Lights />
                     <Environments />
-                    <Physics debug={false} gravity={[0, -1.4, 0]}>
+                    <Physics debug={true} gravity={[0, -1.4, 0]}>
                         <WorldS2/>
                         <Ecctrl
                             camInitDis={-2}
                             camMaxDis={-2}
                             camInitDir= {{x: 0, y: 97}}
-                            position={[0, 0.5, 0]}
+                            position={[
+                                // 0, 0.5, 0
+                                -65, 30 , 55
+                            ]}
                             jumpVel={1}
                             moveImpulsePointY={1}
                             maxVelLimit= {2}
                             springK={0}
                             floatHeight= {0}
-                            sprintJumpMult={10.4}
+                            sprintJumpMult={1.3}
                         >
                             <Avatar scale={0.002} />
                         </Ecctrl>
+                        <Villain2 position={[-65, 29 , 70]} />
                     </Physics>
                     <WelcomeText text={props.text} position={props.position} size={props.size} rotation={props.rotation} />
                 </Suspense>
